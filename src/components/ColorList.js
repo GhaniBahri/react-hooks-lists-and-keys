c312bec660420b98d8f242403f15d2dc96093138
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-09 02:35:32 +0200

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,16 +8,27 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  // const colorElements = colors.map((color) => {
+  //   return <li key={color} style={{ color: color }}>{color}</li>;
+  // });
+  function ColorItem(props) {
+    return <li style={{ color: props.color }}>{props.color}</li>;
+  }
 
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
+        {/* <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li> */}
+        {colorElements} 
+
       </ol>
     </div>
   );
